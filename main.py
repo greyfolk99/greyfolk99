fb9523af3a6fdb05e4f02addda470fb2dea43cf2
greyfolk <greyfolk99@gmail.com>
2023-03-08 14:08:35 +0900

    fixed prompt

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import openai
 import os
-import platform
-# Updated
-
 
 # Get the absolute path of the current file
 current_file = os.path.abspath(__file__)
-
-
 # Get the directory containing the current file
 current_dir = os.path.dirname(current_file)
-
-
 # Get the absolute path of the secrets file
 secrets_file = os.path.join(current_dir, "secrets.txt")
+# Get the absolute path of the readme file
+readme_file = os.path.join(current_dir, "README.md")
 
 
 # Get a quote from GPT3
-def generate_quote():
+def update_quote():
+    # Read the existing file into memory
+    with open(readme_file, "rt", encoding='UTF8') as f:
+        lines = f.readlines()
+
+    # Find the line to update
+    i = 0
+    line_to_update = None
+    for i, line in enumerate(lines):
+        if line.startswith("###### Today's Quote from GPT3:"):
+            i += 1
+            line_to_update = lines[i]
+            break
+
     # Read the API key from the secrets file
     with open(secrets_file, "rt", encoding='UTF8') as f:
         api_key = f.readline().strip()
@@ -25,38 +33,27 @@ def generate_quote():
     # Authenticate to the OpenAI API
     openai.api_key = api_key
 
+    # Get the quote from GPT3
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt="Please generate a motivational quote in one line: ",
+        prompt="Please generate a motivational quote in one line, except " + line_to_update,
         max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.5,
     )
+
     message = response["choices"][0]["text"]
-    return message
 
-
-# Get the absolute path of the readme file
-readme_file = os.path.join(current_dir, "README.md")
-
-
-def update_readme(quote):
-    # Read the existing file into memory
-    with open(readme_file, "rt", encoding='UTF8') as f:
-        lines = f.readlines()
-
-    # Find the line to update
-    for i, line in enumerate(lines):
-        if line.startswith("###### Today's Quote from GPT3:"):
-            lines[i + 1] = "> " + quote.strip().replace("\n", " ") + "\n"
-            break
+    lines[i] = "> " + message.strip().replace("\n", " ") + "\n"
 
     # Write the updated file to disk
     with open(readme_file, "w", encoding='UTF8') as f:
         f.writelines(lines)
 
+    return message
+
 
 # Commit and push the file to GitHub
-quote = generate_quote()
-update_readme(quote)
+quote = update_quote()
+print(quote)
